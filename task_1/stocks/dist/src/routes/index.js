42a0f07c7320bd1059f5ccef966b83c71bbf97a4
w31loa <alexslava.barannik@gmail.com>
2024-11-19 23:32:56 +0500

    rm: dist

--- task_1/stocks/dist/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import ProductRoutes from './product.router.js';
-import StockRoutes from './stock.router.js';
-import { Router } from 'express';
-const router = Router();
-router.use('/api/product', ProductRoutes);
-router.use('/api/stock', StockRoutes);
-export default router;
